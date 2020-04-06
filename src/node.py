93122a8edc51d41dbc4b2760b2b2a89e71b4193f
Li-Cheng Zheng <ny8859884@gmail.com>
2020-04-06 14:06:17 +0800

    [add] ready, unscheduled and finished for qeurying the status of a node

--- src/node.py
+++ src/node.py
@@ -5,11 +5,27 @@ class Node:
         self.id = id
         self.type = type_
         self.fanins = []
+        self.operations = []
         self.fanouts = []
         self.level = -1
-    
+        # -1: unscheduled, 0: running, 1: scheduled
+        self.status = -1 
+        self.remaining_time = 0
+
     def __str__(self):
         return str(self.id)
+
+    def ready(self):
+        for fanin in self.fanins:
+            if fanin.status != 1:
+                return False
+        return True
+
+    def unscheduled(self):
+        return True if self.status == -1 else False
+
+    def finished(self):
+        return True if self.status == 1 else False 
 
     def information(self):
         print('[ Node: %d ]' % self.id)
