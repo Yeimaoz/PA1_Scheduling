b8af2ea2309b30aeb81a5c63ec8dd47f7d7f5b1c
Li-Cheng Zheng <ny8859884@gmail.com>
2020-04-06 14:01:18 +0800

    [add] add_nop, connect all PIs & POs to nop

--- src/graph.py
+++ src/graph.py
@@ -13,6 +13,12 @@ class Graph(list):
     def __setitem__(self, index, value):
         super(Graph, self).__setitem__(index, value)
 
+    def add_nop(self):
+        self[0] = Node(0, 4)
+        self[-1] = Node(-1, 4)
+        [self[0].fanouts.append(PI) for PI in self.PIs]
+        [self[-1].fanins.append(PO) for PO in self.POs]
+
     def connect_from_source_to_sink(self, source, sink):
         self[source].fanouts.append(self[sink])
         self[sink].fanins.append(self[source])
